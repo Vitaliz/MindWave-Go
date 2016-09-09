5731e6862dbd74531a0408ee4fa04d6d52d328d4
Roberto <roberto@TitosNotebook.titos.home>
2016-09-08 23:40:41 -0300

    Agrega asic eeg power

--- mindwave.go
+++ mindwave.go
@@ -5,14 +5,16 @@ import (
     "errors"
     "fmt"
     "sync"
+    "io"
     "github.com/tarm/serial"
 )
 
 var ErrHeadsetNotFound error = errors.New( "Headset no encontrado" )
-var ErrNoHeadsetFound error = errors.New( "No hay headset disponibles" )
+var ErrNoHeadsetFound error = errors.New( "No hay Headset disponibles" )
 var ErrDisconnected error = errors.New( "Desconectado" )
-var ErrRequestDenied error = errors.New( "Requerimiento denegado" )
-var ErrChecksum error = errors.New( "Error de chechsum" )
+var ErrRequestDenied error = errors.New( "Requerimiento Denegado" )
+var ErrChecksum error = errors.New( "Error de Checksum" )
+var ErrZeroPlength error = errors.New( "Tamaño de payload es Cero" )
 
 type MindWave struct {
     port string
@@ -26,7 +28,32 @@ type MindWave struct {
     poorSignalQuality byte
     attentionESense byte
     meditationESense byte
+    blinkStrength byte
     rawWave16Bit int16
+    delta uint32
+    theta uint32
+    lowAlpha uint32
+    highAlpha uint32
+    lowBeta uint32
+    highBeta uint32
+    lowGamma uint32
+    midGamma uint32
+}
+
+type MindWaveData struct {
+    PoorSignalQuality byte
+    AttentionESense byte
+    MeditationESense byte
+    BlinkStrength byte
+    RawWave16Bit int16
+    Delta uint32
+    Theta uint32
+    LowAlpha uint32
+    HighAlpha uint32
+    LowBeta uint32
+    HighBeta uint32
+    LowGamma uint32
+    MidGamma uint32
 }
 
 func NewMindWave( port string, ghid_high byte, ghid_low byte ) *MindWave {
@@ -43,14 +70,14 @@ func NewMindWave( port string, ghid_high byte, ghid_low byte ) *MindWave {
 
 func ( mw *MindWave ) Connect() error {
     if( mw.connected ) {
-        fmt.Println( "MindWave Connect(): ya se encuentra conectado a ", mw.port )
+        fmt.Println( "MindWave Connect(): Ya se encuentra conectado a ", mw.port )
         return nil
     }
 
-    fmt.Print( "MindWave Connect(): intentando conectar a ", mw.port, " => " )
+    fmt.Print( "MindWave Connect(): Intentando conectar a ", mw.port, " => " )
     var err error
     var conn *serial.Port
-    var c *serial.Config = &serial.Config { Name: mw.port, Baud: 115200, ReadTimeout: time.Millisecond * 500 }
+    var c *serial.Config = &serial.Config { Name: mw.port, Baud: 115200, ReadTimeout: time.Millisecond * 1 }
     conn, err = serial.OpenPort( c )
     if( err != nil ) {
         fmt.Println( " ", err )
@@ -58,7 +85,7 @@ func ( mw *MindWave ) Connect() error {
     }
     fmt.Println( "OK" )
 
-    // desconecta y descarta lo recibido
+    // resetea conexión anterior
     fmt.Print( "MindWave Connect(): Limpiando conexión previa => " )
     _, err = conn.Write( []byte { 0xc1 } )
     if( err != nil ) {
@@ -123,7 +150,7 @@ func ( mw *MindWave ) Connect() error {
             if( payload[2] == 0x00 ) {
                 break
             } else {                        // searching
-                time.Sleep( time.Millisecond * 1000 )
+                time.Sleep( time.Millisecond * 1 )
             }
         } else {
             break
@@ -139,29 +166,29 @@ func ( mw *MindWave ) Connect() error {
     fmt.Println( " OK" )
 
     mw.connected = true
+    fmt.Println( "MindWave Connect(): Levantando tarea de lectura de datos" )
     go func( mw *MindWave ) {
         for {
             select {
                 case <- mw.quit:
                     return
                 default:
-                    err := mw.parsePayload()
+                    err = mw.parsePayload()
                     if( err != nil ) {
-                        //fmt.Println( "\nMindWave: ", err )
+                        fmt.Println( "MindWave: ", err )
                     }
                     // requerido para el scheduler
                     time.Sleep( time.Millisecond * 1 )
             }
         }
     }( mw )
-    fmt.Println( "MindWave Connect(): Levantando tarea de lectura de datos" )
     time.Sleep( time.Millisecond * 1000 )
     return nil
 }
 
 func ( mw *MindWave ) Disconnect() {
     if( mw.connected ) {
-        fmt.Print( "MindWave Disconnect(): Deteniendo Tareas => " )
+        fmt.Print( "MindWave Disconnect(): Deteniendo Tarea => " )
         mw.quit <- true
         time.Sleep( time.Millisecond * 1000 )
         fmt.Println( "OK" )
@@ -187,28 +214,25 @@ func ( mw *MindWave ) GlobalHeadsetID() string {
     return fmt.Sprintf( "%02X%02X", mw.ghid_high, mw.ghid_low )
 }
 
-func ( mw *MindWave ) PoorSignalQuality() byte {
+func ( mw *MindWave ) MindWaveData() MindWaveData {
     defer mw.mutex.Unlock()
     mw.mutex.Lock()
-    return mw.poorSignalQuality
-}
-
-func ( mw *MindWave ) AttentionESense() byte {
-    defer mw.mutex.Unlock()
-    mw.mutex.Lock()
-    return mw.attentionESense
-}
-
-func ( mw *MindWave ) MeditationESense() byte {
-    defer mw.mutex.Unlock()
-    mw.mutex.Lock()
-    return mw.meditationESense
-}
-
-func ( mw *MindWave ) RawWave16Bit() int16 {
-    defer mw.mutex.Unlock()
-    mw.mutex.Lock()
-    return mw.rawWave16Bit
+    var mwd MindWaveData = MindWaveData {
+        PoorSignalQuality: mw.poorSignalQuality,
+        AttentionESense: mw.attentionESense,
+        MeditationESense: mw.meditationESense,
+        BlinkStrength: mw.blinkStrength,
+        RawWave16Bit: mw.rawWave16Bit,
+        Delta: mw.delta,
+        Theta: mw.theta,
+        LowAlpha: mw.lowAlpha,
+        HighAlpha: mw.highAlpha,
+        LowBeta: mw.lowBeta,
+        HighBeta: mw.highBeta,
+        LowGamma: mw.lowGamma,
+        MidGamma: mw.midGamma,
+    }
+    return mwd
 }
 
 func ( mw *MindWave ) parsePacket() ( []byte, error ) {
@@ -246,6 +270,10 @@ func ( mw *MindWave ) parsePacket() ( []byte, error ) {
         }
     }
 
+    if( plength <= 0 ) {
+        return nil, ErrZeroPlength
+    }
+
     var payload []byte = make( []byte, plength )
     err = mw.read( payload )
     if( err!=nil ) {
@@ -256,9 +284,7 @@ func ( mw *MindWave ) parsePacket() ( []byte, error ) {
     if( err != nil ) {
         return nil, err
     }
-    var checksum int
-    checksum = int( b[0] )
-
+    var checksum int = int( b[0] )
     var sum int = 0
     for i:=0; i<plength; i++ {
         sum = sum + int( payload[i] )
@@ -279,7 +305,6 @@ func ( mw *MindWave ) parsePayload() error {
     if( err!= nil ) {
         return err
     }
-    //fmt.Printf( "MindWave payload: [%x]\n", payload )
 
     if( payload[0] == 0xd2 ) {      // disconnected
         return ErrDisconnected
@@ -288,7 +313,10 @@ func ( mw *MindWave ) parsePayload() error {
         return nil
     }
 
-    var length int
+    defer mw.mutex.Unlock()
+    mw.mutex.Lock()
+
+    var vlength int
     var pos int = 0
     for pos<len(payload) {
         var exCodeLevel int = 0
@@ -298,49 +326,52 @@ func ( mw *MindWave ) parsePayload() error {
         }
         var code int = int( payload[pos] )
         pos = pos + 1
-        if( code & 0x80 == 0x80 ) {
-            length = int( payload[pos] )
+        if( code >= 0x80 ) {
+            vlength = int( payload[pos] )
             pos = pos + 1
         } else {
-            length = 1
+            vlength = 1
         }
 
-        var data []byte = make( []byte, length )
-        for i:=0; i<length; i=i+1 {
+        var data []byte = make( []byte, vlength )
+        for i:=0; i<vlength; i=i+1 {
             data[i] = payload[pos + i]
         }
-        pos = pos + length
+        pos = pos + vlength
 
         if( exCodeLevel == 0 ) {
             switch code {
-                case 0x02:  // poor signal quality (0 to 255)
-                    mw.mutex.Lock()
+                case 0x02:  // poor signal quality (0 to 255) 200 => no skin contact
                     mw.poorSignalQuality = data[0]
-                    mw.mutex.Unlock()
-                case 0x03:  // heart rate (0 to 255)
-                case 0x04:  // attention eSense (0 to 100)
-                    //fmt.Printf( "Payload: [%x], ExCodeLevel: %02x, Code: %02x [%x]\n", payload, exCodeLevel, code, data )
-                    mw.mutex.Lock()
+                case 0x04:  // attention eSense (0 to 100) 40-60 => neutral, 0 => result is unreliable
                     mw.attentionESense = data[0]
-                    mw.mutex.Unlock()
-                case 0x05:  // meditation eSense (0 to 100)
-                    //fmt.Printf( "Payload: [%x], ExCodeLevel: %02x, Code: %02x [%x]\n", payload, exCodeLevel, code, data )
-                    mw.mutex.Lock()
+                case 0x05:  // meditation eSense (0 to 100) 40-60 => neutral, 0 => result is unreliable
                     mw.meditationESense = data[0]
-                    mw.mutex.Unlock()
-                case 0x06:  // 8bit raw wave value (0 to 255)
-                case 0x07:  // raw marker section start (0)
+                case 0x16:  // blink strength (1 to 255)
+                    mw.blinkStrength = data[0]
                 case 0x80:  // raw wave value (-32768 to 32767) - big endian
-                    mw.mutex.Lock()
-                    var n int32 = ( int32( data[0] )<<8 ) + int32( data[1] )
-                    if( ( n & 0x8000 ) != 0 ) {
-                        n = n - 0xFFFF - 1
+                    var n int32 = int32( data[0] )<<8 + int32( data[1] )
+                    if( n >= 32768 ) {
+                        n = n - 65536
                     }
                     mw.rawWave16Bit = int16( n )
-                    mw.mutex.Unlock()
-                case 0x81:  // eeg power struct
-                case 0x83:  // asic eeg power struct
-                case 0x86:  // rrinterval (0 to 65535)
+                case 0x83:  // asic eeg power struct (8, 3 bytes unsigned int big indian)
+                    mw.delta = uint32( data[0] )<<16 + uint32( data[1] )<<8 + uint32( data[2] )
+                    mw.theta = uint32( data[3] )<<16 + uint32( data[4] )<<8 + uint32( data[5] )
+                    mw.lowAlpha = uint32( data[6] )<<16 + uint32( data[7] )<<8 + uint32( data[7] )
+                    mw.highAlpha = uint32( data[9] )<<16 + uint32( data[10] )<<8 + uint32( data[11] )
+                    mw.lowBeta = uint32( data[12] )<<16 + uint32( data[13] )<<8 + uint32( data[14] )
+                    mw.highBeta = uint32( data[15] )<<16 + uint32( data[16] )<<8 + uint32( data[17] )
+                    mw.lowGamma = uint32( data[18] )<<16 + uint32( data[19] )<<8 + uint32( data[20] )
+                    mw.midGamma = uint32( data[21] )<<16 + uint32( data[22] )<<8 + uint32( data[23] )
+                // case 0x01:  // code battery - battery low (0x00)
+                // case 0x03:  // heart rate (0 to 255)
+                // case 0x06:  // 8bit raw wave value (0 to 255)
+                // case 0x07:  // raw marker section start (0)
+                // case 0x81:  // eeg power struct (legacy float)
+                // case 0x86:  // rrinterval (0 to 65535)
+                default:
+                    fmt.Printf( "ExCodeLevel: %02x, Code: %02x, Data: [%02x]\n", exCodeLevel, code, data )
             }
         }
     }
@@ -353,18 +384,23 @@ func ( mw *MindWave ) read( data[] byte ) error {
     var pos, n int
     var err error
     var nbytes int = len( data )
+    var tries int = 0
 
-    for pos=0; pos < nbytes; {
+    for pos=0; pos < nbytes && tries < 500; {
         n, err = mw.conn.Read( b )
-        if( err != nil ) {
+        if( err != nil && err != io.EOF ) {
             return err
         }
-        if( n != 1 ){
-            time.Sleep( time.Millisecond * 1 )
+        if( n == 0 ){
+            tries = tries + 1
             continue
         }
         data[pos] = b[0]
         pos = pos + 1
+        tries = 0
+    }
+    if( pos < nbytes ) {
+        return io.EOF
     }
     return nil
 }
--- samples/main.go
+++ samples/main.go
@@ -8,14 +8,15 @@ import (
 
 func main() {
     // colocar el headset unos 4 minutos antes para que se estabilice
+    // el Global Headset Unique Identifier está en la zona de la batería
     mw := mindwave.NewMindWave( "/dev/ttyUSB0", 0xF6, 0x4F )
     err := mw.Connect()
     if( err == nil ) {
         for i:=0; i<=30000; i=i+1 {
+            fmt.Println( mw.GlobalHeadsetID(), mw.MindWaveData() )
+
             // requerido para el scheduler
             time.Sleep( time.Millisecond * 1 )
-            fmt.Printf( "%06d ==> Signal: %03d, Attention: %03d, Meditation: %03d, RawWave: %d\n",
-                        i, mw.PoorSignalQuality(), mw.AttentionESense(), mw.MeditationESense(), mw.RawWave16Bit() )
         }
         mw.Disconnect()
     }
